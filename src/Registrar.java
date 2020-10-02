72e75f2750b1bd29805115928ca70c72b4633cc2
Hishmat Salehi <salehh6@mcmaster.ca>
2020-10-02 19:40:22 -0400

    Finished part 1 of assignment 2.

--- src/Registrar.java
+++ src/Registrar.java
@@ -1,13 +1,44 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Registrar {
     ArrayList<Student> students;
     ArrayList<Course> courses;
+    HashMap<Course, ArrayList<Course>> requiredCourses;
 
     public Registrar() {
         students = new ArrayList<>();
         courses = new ArrayList<>();
+        requiredCourses = new HashMap<>();
+    }
+
+    /**
+     * Finds the Student with the given studentID
+     * @param studentID
+     * @return Student object
+     */
+    public Student getStudent(int studentID) {
+        for (Student student : students) {
+            if (student.getStudentID() == studentID) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Finds the Course with the given courseName
+     * @param courseName
+     * @return Course object
+     */
+    public Course getCourse(String courseName) {
+        for (Course course : courses) {
+            if (course.getUniqueName().equals(courseName)) {
+                return course;
+            }
+        }
+        return null;
     }
 
     /* Hardcoded example. Instead, you need to implement new functions that read
@@ -64,19 +95,19 @@ public class Registrar {
             command = inputList[0];
 
             switch(command) {
-                // Course command
+                // COURSE courseName
                 case "COURSE":
                     // add the course to the list of courses
                     courses.add(new Course(inputList[1]));
                     break;
 
-                // Student command
+                // STUDENT studentName studentID
                 case "STUDENT":
                     // add the student to the list of students
                     students.add(new Student(inputList[1], Integer.parseInt(inputList[2])));
                     break;
 
-                // Section command
+                // SECTION courseName section maxCapacity
                 case "SECTION":
                     // add the section to the course sections
                     for (Course course : courses) {
@@ -86,17 +117,34 @@ public class Registrar {
                     }
                     break;
 
-                // Enroll command
+                // ENROLL studentID CourseName section
                 case "ENROLL":
+                    // find the student
+                    Student student = getStudent(Integer.parseInt(inputList[1]));
 
+                    // find the course
+                    Course course = getCourse(inputList[2]);
+
+                    // enroll the student in the appropriate section
+                    boolean enrollmentResult = course.enrollStudent(student, inputList[3]);
+                    TerminalPrinter.printEnrollmentResult(enrollmentResult, student.getName(),
+                            course.getUniqueName(), inputList[3]);
                     break;
 
-                // UnEnroll command
+                // UNENROLL studentID CourseName section
                 case "UNENROLL":
+                    // find the student
+                    Student student1 = getStudent(Integer.parseInt(inputList[1]));
 
+                    // find the course
+                    Course course1 = getCourse(inputList[2]);
+
+                    boolean unenrollmentResult = course1.unenrollStudent(student1, inputList[3]);
+                    TerminalPrinter.printUnenrollmentResult(unenrollmentResult, student1.getName(),
+                            course1.getUniqueName(), inputList[3]);
                     break;
 
-                // Requirement command
+                // REQUIREMENT courseName requiredCourseName
                 case "REQUIREMENT":
 
                     break;
