d75f401686e0bbe15c3688c87b30ee14bc753471
Hishmat Salehi <salehh6@mcmaster.ca>
2020-10-02 18:19:25 -0400

    Added the pdf file.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Registrar registrar = new Registrar();
-        registrar.runExampleCommands();
-//        registrar.run();
+//        registrar.runExampleCommands();
+        registrar.run();
     }
 }
