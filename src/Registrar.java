87ddc0e746a35f1233ae95905fb8109dab3234ef
Hishmat Salehi <salehh6@mcmaster.ca>
2020-10-02 18:13:47 -0400

    Finshed implementing COURSE, STUDENT and SECTION commands.

--- src/Registrar.java
+++ src/Registrar.java
@@ -66,45 +66,46 @@ public class Registrar {
             switch(command) {
                 // Course command
                 case "COURSE":
-
-
-                    inputList = sc.nextLine().split(" ");
+                    // add the course to the list of courses
+                    courses.add(new Course(inputList[1]));
                     break;
 
                 // Student command
                 case "STUDENT":
-
-                    inputList = sc.nextLine().split(" ");
+                    // add the student to the list of students
+                    students.add(new Student(inputList[1], Integer.parseInt(inputList[2])));
                     break;
 
                 // Section command
                 case "SECTION":
-
-                    inputList = sc.nextLine().split(" ");
+                    // add the section to the course sections
+                    for (Course course : courses) {
+                        if (course.getUniqueName().equals(inputList[1])) {
+                            course.addSection(new Section(inputList[2], Integer.parseInt(inputList[3])));
+                        }
+                    }
                     break;
 
                 // Enroll command
                 case "ENROLL":
 
-                    inputList = sc.nextLine().split(" ");
                     break;
 
                 // UnEnroll command
                 case "UNENROLL":
 
-                    inputList = sc.nextLine().split(" ");
                     break;
 
                 // Requirement command
                 case "REQUIREMENT":
 
-                    inputList = sc.nextLine().split(" ");
                     break;
 
             }
+            // set the nextLine as the new command
+            inputList = sc.nextLine().split(" ");
 
-
-        } while (!inputList[0].equals("FINISH"));
+        } while (!inputList[0].equals("FINISH")); // While the input is not finished, keep reading commands
 
     }
 }
